6bfb5d03b989ca106bcdcfa61bb150e3b3afc0c4
Tyler Boyd <tylerb@theboydprojects.com>
2020-09-01 19:48:17 -0500

    removed extra code from scaffold. Added catcher component

---
+++ components/Catcher.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-container>
+      <v-row class="my-16" justify="center" align="center">
+        <v-col cols="10" md="8">
+          <div class="header-box">
+            <p class="header text-center text-h3 text-md-h1">Welcome Home.</p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header {
+  font-weight: 900;
+  font-size: 120px;
+}
+.header-box {
+    height: 900px;
+}
+</style>
