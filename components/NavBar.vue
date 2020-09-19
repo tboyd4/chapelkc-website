94538e599f7bc1f4ef7d65a805a33f2e34bf8562
TylerBoyd <tboyd1898@gmail.com>
2020-09-19 17:22:11 -0500

    added header

---
+++ components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar app color="white" height="100" hide-on-scroll>
+    <v-avatar class="mr-3" color="grey lighten-5" size="70">
+      <v-img
+        contain
+        max-height="70%"
+        src="/ckclogo.png"
+      ></v-img>
+    </v-avatar>
+
+    <v-toolbar-title class="font-weight-black headline">
+      
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
+    <nav-bar></nav-bar>
     <catcher></catcher>
     <video-sermon></video-sermon>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue'
 import Catcher from '../components/Catcher.vue'
 import VideoSermon from '../components/VideoSermon.vue'
 export default {
   components: {
     Catcher,
-    VideoSermon
+    VideoSermon,
+    NavBar
   }
 }
 </script>
